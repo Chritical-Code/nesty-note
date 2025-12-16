ab8d68b46260db880af96e397aa0719455aab168
chritical <christopherkbanks@gmail.com>
2025-12-16 17:38:05 -0500

    Reconfigured note controls, allowing for more than just editing.

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,4 +1,5 @@
 import styles from "./Note.module.css";
+import { useEffect, useState, useRef } from 'react';
 
 type NoteProps = {
     text: string;
@@ -10,19 +11,46 @@ type NoteProps = {
 }
 
 export default function Note({text, editNote, inKey, setFocusedTextArea, redraw, textArea}: NoteProps){
+    const [mouseMode, setMouseMode] = useState("none");
+    const timer = useRef<number | undefined>(undefined);
+    
+    useEffect(() => {
+        //
+
+        return () => {
+            setMouseMode("none");
+            clearTimeout(timer.current);
+        }
+    }, []);
+
     function handleChange(event: React.ChangeEvent<HTMLTextAreaElement>){
         editNote(event.target.value, inKey);
     }
 
-    function handleClick(){
-        setFocusedTextArea(textArea.current);
-        redraw();
+    function handleMouseDown(){
+        setMouseMode("edit")
+        timer.current = setTimeout(() => {
+            setMouseMode("drag");
+        }, 500);
+    }
+
+    function handleMouseUp(){
+        if(mouseMode == "edit"){
+            setFocusedTextArea(textArea.current);
+            redraw();
+            setMouseMode("none");
+            clearTimeout(timer.current);
+        }
+        else if(mouseMode == "drag"){
+            setMouseMode("none");
+            clearTimeout(timer.current);
+        }
     }
     
     return(
         <div className={styles.stickyNote}>
             <textarea className={styles.textArea} defaultValue={text} ref={textArea} onChange={(event) => handleChange(event)}></textarea>
-            <div className={styles.noteControl} onClick={() => handleClick()}></div>
+            <div className={styles.noteControl} onMouseDown={() => handleMouseDown()} onMouseUp={() => handleMouseUp()}></div>
         </div>
     );
 }
